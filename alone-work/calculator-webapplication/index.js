2a7a880eed18405636425c5e4a10a4d7d78412a4
Simona <see.moona@yahoo.com>
2020-08-04 18:38:42 +0200

    bmi added

--- alone-work/calculator-webapplication/index.js
+++ alone-work/calculator-webapplication/index.js
@@ -1,24 +1,64 @@
-
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended:true})) // extended posts nested obj
+// Home
+app.get("/", (req, res) => {
+res.send(`Choose between '/calculator' or '/bmicalculator' in the URL`)
+});
+
+app.use(bodyParser.urlencoded({ extended: true })); // extended posts nested obj
 //we use when we try to pass info that comes from a html form
 
-app.get('/', (req,res)=>{
-   // console.log(__dirname + "/index.html");
-    res.sendFile(__dirname + "/index.html");// sendFile for files
-})
-app.post('/',(req,res)=>{
-  //  console.log(req.body)
-    let num1 = Number(req.body.num1); // to turn string in number
-    let num2 = Number(req.body.num2);
-    let result = num1+num2;
-    res.send(`The result is ${result}`)
-})
+//Basic Calculator
+app.get("/calculator", (req, res) => {
+  res.sendFile(__dirname + "/index.html"); // sendFile for files
+});
+app.post("/calculator", (req, res) => {
+  let num1 = Number(req.body.num1); // to turn string in number
+  let num2 = Number(req.body.num2);
+  let result = 0;
+  switch (req.body.select) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
 
-app.listen(port, ()=>{
-    console.log('Listening to localhost:3000')
-})
+}
+  res.send(`The result is ${result}`);
+});
+
+//BMI Calculator router
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html"); // sendFile for files
+});
+
+app.post("/bmicalculator", (req, res) => {
+  let height = parseFloat(req.body.height); // to turn string in number
+  let weight = parseFloat(req.body.weight);
+  let bmi = (weight / Math.pow(height / 100, 2)).toFixed(2);
+  let status = "";
+  if (bmi < 18.5) {
+    status = "Underweight";
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    status = "Normal";
+  } else if (bmi > 25.0 && bmi < 29.9) {
+    status = "Overweight";
+  } else if (bmi >= 30.0) {
+    status = "Obese";
+  }
+  res.send(
+    `Your BMI is: <span style="font-weight: bold; font-size: 1.2rem">${bmi}</span>, your Weight Status is: <span style="font-weight: bold ; color: red; font-size: 1.2rem">${status}</span>`);
+});
+app.listen(port, () => {
+  console.log(`Listening to localhost:${port}`);
+});
