c7189989a656408b600ed8935f2dc2331fdf3a41
Simona <see.moona@yahoo.com>
2020-08-05 10:22:35 +0200

    remove an article

--- class/04.08.20 lowdb/index.js
+++ class/04.08.20 lowdb/index.js
@@ -1,59 +1,101 @@
-const express = require('express');
-const low = require('lowdb');
-const FileSync =  require('lowdb/adapters/FileSync');
+// npm i express
+// npm i lowdb
+
+// express
+const express = require("express");
 const app = express();
-const port = 3000;
-const jsonFile = new FileSync('db.json');
+
+// lowdb
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const jsonFile = new FileSync("db.json");
 const db = low(jsonFile);
 
+// to process the json data
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
-//db init
-app.get('/new',(req, res)=>{
-    db.defaults({articles:[], user:{},num:1}).write();
-    res.send('New db being init');
-})
-//adding new articles
-app.get('/add',(req, res)=>{
-    // url      /add?id=1&title=1+like+water
-    const id = req.query.id;
-    const title = req.query.title;
-    db.get('articles').push({id: id, title: title}).write();
-    res.send(`New data being entered with id =${id} and title=${title}`);
-})
-//look up
-app.get('/find',(req, res)=>{
-    // url/find?id=1
-    const idToFind = req.query.id;
-    res.send(db.get('articles').find({id:idToFind}).value());
-})
+// root req
+app.get("/", (req, res) => {
+  res.send("Welcome to our lowdb");
+});
 
-//update 
-app.get('/update',(req,res)=>{
-    db.update('num', (n)=> n+1).write();
-    const num = db.get('num').value();
-    res.send(`num was increased by 1, num is now ${num}`)
-})
+// db init
+app.get("/new", async (req, res) => {
+  await db.defaults({ articles: [], user: {}, num: 1 }).write();
+  res.send("New db being init");
+});
 
-//user name
-app.get('/user',(req,res)=>{
-   // url /user?name=Hadi
-   const name = req.query.name;
-   db.set('user.name', name).write();
-   res.send(`Hey ${name}`)
-})
+// adding new
+app.get("/add", async (req, res) => {
+  //  url      /add?id=1&title=1+like+water
+  const id = req.query.id;
+  const title = req.query.title;
+  await db.get("articles").push({ id: id, title: title }).write();
+  res.send(`New data being entered with id = ${id} and title = ${title}`);
+});
+// look up
+app.get("/find", async (req, res) => {
+  // url    /find?id=1
+  const idToFind = req.query.id;
+  res.send(await db.get("articles").find({ id: idToFind }).value());
+});
 
-//Post method 
-//url https://website.com/login
-// {
-//  'email':'my@email.co',
-//  'password': '000callme000
-// }
+// update
+app.get("/update", async (req, res) => {
+  // url    /update
+  await db.update("num", (n) => n + 1).write();
+  const num = db.get("num").value();
+  res.send(`num was increased by one, now num is = ${num}`);
+});
 
-app.post('/login', (req,res)=>{
-    const userName = req.body.email;
-    const userPass = req.body.password;
-})
+// user name
+app.get("/user", async (req, res) => {
+  // url   /user?name=Hadi
+  const name = req.query.name;
+  await db.set("user.name", name).write();
+  res.send(`Hey ${name} `);
+});
 
-app.listen(port, ()=>{
-    console.log(`Server listens on http://localhost:${port}`)
-})
+// Post method     url http://localhost:3000/login
+//  {
+//     "userName": "Hadi",
+//      "pass":"000callme000"
+//  }
+//
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  const userName = req.body.userName;
+  const userPass = req.body.pass;
+  if (userName === "Hadi" && userPass === "000callme000") {
+    res.status(200).send(`Welcome ${userName}`);
+  } else {
+    res.status(401).send(`invalid username or pass`);
+  }
+});
+
+// remove
+app.get("/delete", async (req, res) => {
+  // to remove article
+  // url     /delete?title=something
+  //   const title = req.query.title;
+  //   await db.get("articles").remove({ title: title }).write();
+  //   res.status(200).send(`Your ${title} has been removed `);
+
+  // to remove property
+  // url    /delete
+  db.unset("user.name").write();
+  res.status(200).send("user.name has been removed");
+});
+
+app.get("/api", (req, res) => {
+  res.status(200).json({ id: 1, cityName: "Berlin", county: "DE" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server listen on http://localhost:${PORT}`);
+});
