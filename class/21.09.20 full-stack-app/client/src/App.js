ee889d25fb7da9f20441b5a9e266f84ec40533c8
Simona <see.moona@yahoo.com>
2020-09-22 09:48:17 +0200

    local storage

--- class/21.09.20 full-stack-app/client/src/App.js
+++ class/21.09.20 full-stack-app/client/src/App.js
@@ -5,13 +5,31 @@ import './App.css';
 function App() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
-    fetch('/users')
-      .then((result) => result.json())
-      .then((data) => setUsers(data));
+    if (localStorage.getItem('data')) {
+      setUsers(JSON.parse(localStorage.getItem('data')));
+    } else {
+      setUsers([]);
+    }
   }, []);
 
+  const fetchData = () => {
+    fetch('/users')
+      .then((result) => result.json())
+      .then((data) => {
+        localStorage.setItem('data', 'Hadi');
+        setUsers(data);
+      });
+  };
   const usersArr = users.map((user) => <h4 key={user._id}>{user.userName}</h4>);
-  return <div className="App">{usersArr}</div>;
+  return (
+    <div className="App">
+      <h4>Users</h4>
+      <button onClick={fetchData}>
+        {users.length ? 'Update' : 'Get Data'}
+      </button>
+      {usersArr}
+    </div>
+  );
 }
 
 export default App;
